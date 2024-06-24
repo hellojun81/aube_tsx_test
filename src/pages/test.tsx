ed8513ab62f7668e833e5b9c0a082067a8be8a87
김완준 <>
2024-06-24 21:26:05 +0900

    t

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,17 +1,17 @@
 'use client'
 import React, { useRef, useEffect, useState, RefCallback, useCallback } from "react";
-// import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic';
 import { motion, useAnimation, useViewportScroll } from 'framer-motion';
-// import '../app/main/main.css'
-// import "react-nice-scroll/dist/styles.css";
-import EmblaCarousel from '../app/carousel/newCarouse'
+import './main.css'
+import "react-nice-scroll/dist/styles.css";
+import EmblaCarousel from '../carousel/newCarouse'
 // import HorizontalGallery from '../carousel/HorizontalSection'
-import Email from '../app/components/sendEmail'
-import Footer from '../app/components/footer'
-import Naverbar from '../app/components/Navbar'
+import Email from '../components/sendEmail'
+import Footer from '../components/footer'
+import Naverbar from '../components/Navbar'
 import { ScrollContainer } from "react-nice-scroll"
 // import styles from './ScrollIcon.module.scss';
-// const SequenceSection = dynamic(() => import('react-nice-scroll').then(mod => mod.SequenceSection), { ssr: false });
+const SequenceSection = dynamic(() => import('react-nice-scroll').then(mod => mod.SequenceSection), { ssr: false });
 
 const App: React.FC = () => {
   const scrolloptions = {
@@ -40,122 +40,121 @@ const App: React.FC = () => {
   ];
   const Carousel = [
     { floor: 1, loop: 7, classname: floorpadding, id: '1floor' },
-    // { floor: 2, loop: 7, classname: floorpadding, id: '2floor' },
-    // { floor: 3, loop: 7, classname: floorpadding, id: '3floor' },
-    // { floor: 4, loop: 3, classname: floorpadding, id: 'Other' },
-    // { floor: 5, loop: 3, classname: floorpadding, id: 'Stairs' },
-    // { floor: 6, loop: 3, classname: floorpadding, id: 'Outside' },
+    { floor: 2, loop: 7, classname: floorpadding, id: '2floor' },
+    { floor: 3, loop: 7, classname: floorpadding, id: '3floor' },
+    { floor: 4, loop: 3, classname: floorpadding, id: 'Other' },
+    { floor: 5, loop: 3, classname: floorpadding, id: 'Stairs' },
+    { floor: 6, loop: 3, classname: floorpadding, id: 'Outside' },
   ]
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   setIsClient(true); // 클라이언트 측에서만 true로 설정
-  //   const div = divRef.current as HTMLDivElement;
-  //   const elements = document.querySelectorAll('.ns-container');
+    setIsClient(true); // 클라이언트 측에서만 true로 설정
+    const div = divRef.current as HTMLDivElement;
+    const elements = document.querySelectorAll('.ns-container');
 
-  //   elements.forEach(element => {
-  //     (element as HTMLElement).style.overflow = 'inital';
-  //   });
-  //   const windowHeight = window.innerHeight;        // 윈도우의 높이와 너비를 가져옵니다.
-  //   const windowWidth = window.innerWidth;          // div 요소의 높이와 너비를 가져옵니다.
+    elements.forEach(element => {
+      (element as HTMLElement).style.overflow = 'inital';
+    });
+    const windowHeight = window.innerHeight;        // 윈도우의 높이와 너비를 가져옵니다.
+    const windowWidth = window.innerWidth;          // div 요소의 높이와 너비를 가져옵니다.
 
-  //   if (windowHeight < windowWidth) {
-  //     setscreenMode('width')
-  //     setfloorpadding('mainsection2')
-  //     setfirstpage('firstpage2')
-  //     setimagesPath('/images/jpg_width')
-  //   }
+    if (windowHeight < windowWidth) {
+      setscreenMode('width')
+      setfloorpadding('mainsection2')
+      setfirstpage('firstpage2')
+      setimagesPath('/images/jpg_width')
+    }
 
-  //   console.log({ titleRefs: titleRefs.current })
-  //   let index = 0;
+    console.log({ titleRefs: titleRefs.current })
+    let index = 0;
 
 
 
-  //   titleRefs.current.forEach(ref => {
-  //     console.log('titleRefs123')
-  //     index++;
+    titleRefs.current.forEach(ref => {
+      console.log('titleRefs123')
+      index++;
 
-  //     if (ref) {
-  //       const parent = ref.parentElement as HTMLElement | null;
-  //       if (parent) {
-  //         const parentHeight = parent.clientHeight;
-  //         const parentWidth = parent.clientWidth;
-  //         const textHeight = ref.clientHeight;
-  //         let checkindex = index % 2
-  //         let leftposition
-  //         ref.style.position = 'absolute';
-  //         ref.style.top = `${(parentHeight - textHeight) / 2}px`;
-  //         ref.style.left = `${leftposition}px`;
-  //         let childWidth = titleRefs.current[0]?.clientWidth
-  //         console.log('parent', parent)
-  //         if (childWidth) {
-  //           leftposition = (parentWidth - childWidth) / 2
-  //           if (index !== 5) {
-  //             if (checkindex == 1) {
-  //               ref.style.left = `${leftposition}px`;
-  //             } else {
-  //               ref.style.right = `${leftposition}px`;
-  //             }
-  //           }
-  //         }
+      if (ref) {
+        const parent = ref.parentElement as HTMLElement | null;
+        if (parent) {
+          const parentHeight = parent.clientHeight;
+          const parentWidth = parent.clientWidth;
+          const textHeight = ref.clientHeight;
+          let checkindex = index % 2
+          let leftposition
+          ref.style.position = 'absolute';
+          ref.style.top = `${(parentHeight - textHeight) / 2}px`;
+          ref.style.left = `${leftposition}px`;
+          let childWidth = titleRefs.current[0]?.clientWidth
+          console.log('parent', parent)
+          if (childWidth) {
+            leftposition = (parentWidth - childWidth) / 2
+            if (index !== 5) {
+              if (checkindex == 1) {
+                ref.style.left = `${leftposition}px`;
+              } else {
+                ref.style.right = `${leftposition}px`;
+              }
+            }
+          }
 
-  //       }
-  //     }
-  //   });
-  //   const updateRotation = () => {
-  //     const scrollValue = scrollY.get();
-  //     if (scrollValue === 0) {
-  //       // 스크롤이 원점으로 돌아왔을 때 초기 상태로 되돌리기
-  //       controls1.start({ rotate: -45, transition: { duration: 0.5 } });
-  //       controls2.start({ rotate: 45, transition: { duration: 0.5 } });
-  //       controls3.start({ rotate: 45, transition: { duration: 0.5 } });
-  //       controls4.start({ rotate: -45, transition: { duration: 0.5 } });
-  //     } else {
-  //       controls1.start({
-  //         rotate: 45,
-  //         transition: { duration: 2.5 }
-  //       });
-  //       controls2.start({
-  //         rotate: -45,
-  //         transition: { duration: 2.5 }
-  //       });
-  //       controls3.start({
-  //         rotate: -45,
-  //         transition: { duration: 2.5 }
-  //       });
-  //       controls4.start({
-  //         rotate: 45,
-  //         transition: { duration: 2.5 }
-  //       });
-  //     }
-  //   };
+        }
+      }
+    });
+    const updateRotation = () => {
+      const scrollValue = scrollY.get();
+      if (scrollValue === 0) {
+        // 스크롤이 원점으로 돌아왔을 때 초기 상태로 되돌리기
+        controls1.start({ rotate: -45, transition: { duration: 0.5 } });
+        controls2.start({ rotate: 45, transition: { duration: 0.5 } });
+        controls3.start({ rotate: 45, transition: { duration: 0.5 } });
+        controls4.start({ rotate: -45, transition: { duration: 0.5 } });
+      } else {
+        controls1.start({
+          rotate: 45,
+          transition: { duration: 2.5 }
+        });
+        controls2.start({
+          rotate: -45,
+          transition: { duration: 2.5 }
+        });
+        controls3.start({
+          rotate: -45,
+          transition: { duration: 2.5 }
+        });
+        controls4.start({
+          rotate: 45,
+          transition: { duration: 2.5 }
+        });
+      }
+    };
 
-  //   if (isClient) {
-  //     const unsubscribeScroll = scrollY.onChange(updateRotation);
-  //     return () => {
-  //       unsubscribeScroll();
-  //     };
-  //   }
-  // }, [controls1, controls2, controls3, controls4, scrollY, isClient]);
+    if (isClient) {
+      const unsubscribeScroll = scrollY.onChange(updateRotation);
+      return () => {
+        unsubscribeScroll();
+      };
+    }
+  }, [controls1, controls2, controls3, controls4, scrollY, isClient]);
 
-  // if (!isClient) {
-  //   // 서버 측에서는 아무것도 렌더링하지 않음
-  //   return null;
-  // }
-  // const setTitleRef = (index: number): RefCallback<HTMLDivElement> => {
-  //   return (el) => {
-  //     titleRefs.current[index] = el;
-  //   };
-  // };
+  if (!isClient) {
+    // 서버 측에서는 아무것도 렌더링하지 않음
+    return null;
+  }
+  const setTitleRef = (index: number): RefCallback<HTMLDivElement> => {
+    return (el) => {
+      titleRefs.current[index] = el;
+    };
+  };
 
   return (
     <>
       <div>
-        {/* <Naverbar /> */}
+        <Naverbar />
         {/* <ScrollContainer > */}
           <></>
-       
-            {/*    <section
+          <section
             id='home'
             className={firstpage}
             style={{
@@ -164,7 +163,7 @@ const App: React.FC = () => {
               position: 'relative',
               backgroundColor: '#171717',
               zIndex: '99',
-              height: '200vh'
+
             }}
           >
 
@@ -207,15 +206,15 @@ const App: React.FC = () => {
               end="80%"
               imagesPath={imagesPath}
               imagesCount={30}
-              imagesType="jpg" />
+              imagesType="jpg" /> */}
           </section>
 
           {/* </ScrollContainer> */}
-          <div
+          <section
             id='floor0'
             className={floorpadding}
             style={{
-              height: "180vh",
+            
               // position: 'relative'
             }}
           >
@@ -229,28 +228,9 @@ const App: React.FC = () => {
               Aube Studio 건축물은 1974년 공장으로 시작되었습니다. 이 흥미로운 건축물은 각각 다른 양식으로 1986년에 2층, 2013년 3층이 증축되어 방문자를 시간의 회랑으로 초대합니다. 산업화를 상징하는 붉은 벽돌 위에 근대 건축의 거장 르코르뷔제를 오마주 하는 창문과 문들은 기능주의적이면서 유기적인 조형적 아름다움을 선사하며, 710㎡의 대지 위에 3층으로 지어진 두 개의 건물과 마당은 거의 모든 것을 하기에 특별한 공간으로 제공됩니다.<br /><br /> Aube Studio began as a factory in 1974. This interesting building, each in a different style, was expanded with a second floor in 1986 and a third floor in 2013, inviting visitors to explore the corridors of time. Windows and doors that pay homage to the master of modern architecture, Le Corbusier, on red bricks symbolizing industrialization present a functionalistic yet organic formative beauty, and the two three-story buildings and yard built on a 710㎡ site provide almost everything. Therefore, it is provided as a special space.
             </p>
 
-          </div>
-          <div
-            id='floor0'
-            className={floorpadding}
-            style={{
-              height: "180vh",
-              // position: 'relative'
-            }}
-          >
-            <div style={{
-              fontSize: '7em',
-              fontWeight: '700',
-              textAlign: 'center',
-              paddingTop: '10px'
-            }}>AUBE</div>
-            <p>
-              Aube Studio 건축물은 1974년 공장으로 시작되었습니다. 이 흥미로운 건축물은 각각 다른 양식으로 1986년에 2층, 2013년 3층이 증축되어 방문자를 시간의 회랑으로 초대합니다. 산업화를 상징하는 붉은 벽돌 위에 근대 건축의 거장 르코르뷔제를 오마주 하는 창문과 문들은 기능주의적이면서 유기적인 조형적 아름다움을 선사하며, 710㎡의 대지 위에 3층으로 지어진 두 개의 건물과 마당은 거의 모든 것을 하기에 특별한 공간으로 제공됩니다.<br /><br /> Aube Studio began as a factory in 1974. This interesting building, each in a different style, was expanded with a second floor in 1986 and a third floor in 2013, inviting visitors to explore the corridors of time. Windows and doors that pay homage to the master of modern architecture, Le Corbusier, on red bricks symbolizing industrialization present a functionalistic yet organic formative beauty, and the two three-story buildings and yard built on a 710㎡ site provide almost everything. Therefore, it is provided as a special space.
-            </p>
+          </section>
 
-          </div>
-
-        {/* {Carousel.map((link, index) => (
+          {Carousel.map((link, index) => (
             <section
               // id={link.id}
               // className={floorpadding}
@@ -269,10 +249,27 @@ const App: React.FC = () => {
                 key={index}
               />
             </section>
-          ))}   */}
+          ))}
 
-       
- 
+          <section
+            id='Contact'
+            style={{
+         
+              marginBottom:"100px"
+            }}
+            className={floorpadding}
+          >
+            <div><h1>Contact US</h1></div>
+            <Email />
+          </section>
+
+          <section
+            style={{
+
+            }}
+          >
+            {/* <Footer /> */}
+          </section>
    
       </div>
     </>
